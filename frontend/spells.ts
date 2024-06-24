543e47adae375f6592ffd89e80b9c8e017a30ab5
raylu <90059+raylu@users.noreply.github.com>
2024-06-23 21:19:08 -0700

    drag spell icons

--- frontend/spells.ts
+++ frontend/spells.ts
@@ -1,4 +1,4 @@
-import {type PointerEvent, ScreenElement, vec} from 'excalibur';
+import {type PointerEvent, ScreenElement, vec, type Vector} from 'excalibur';
 import {spellIcons} from './sprites';
 
 const tooltip = document.querySelector<HTMLElement>('tooltip')!;
@@ -6,11 +6,19 @@ const tooltip = document.querySelector<HTMLElement>('tooltip')!;
 class Spell {
 	name: string;
 	icon: ScreenElement;
+	iconPos: Vector;
+	dragging: boolean = false;
 	constructor(name: string, iconX: number, iconY: number) {
 		this.name = name;
 
-		this.icon = new ScreenElement({scale: vec(2, 2)});
-		this.icon.graphics.use(spellIcons.getSprite(iconX, iconY));
+		const iconSprite = spellIcons.getSprite(iconX, iconY);
+		this.icon = new ScreenElement({
+			width: iconSprite.width,
+			height: iconSprite.height,
+			scale: vec(2, 2),
+			anchor: vec(0.5, 0.5),
+		});
+		this.icon.graphics.use(iconSprite);
 		this.icon.pointer.useGraphicsBounds = true;
 		this.icon.on('pointerenter', () => {
 			tooltip.innerText = name;
@@ -19,13 +27,24 @@ class Spell {
 		this.icon.on('pointerleave', () => {
 			tooltip.style.opacity = '0';
 		});
+		this.icon.on('pointerdragstart', () => {
+			this.dragging = true;
+		});
+		this.icon.on('pointerdragend', () => {
+			this.dragging = false;
+			this.icon.actions.moveTo(this.iconPos, 1000);
+		});
 		this.icon.on('pointermove', (event: PointerEvent) => {
 			tooltip.style.top = event.pagePos.y - 100 + 'px';
 			tooltip.style.left = event.pagePos.x + 'px';
+			if (this.dragging)
+				this.icon.pos = event.screenPos;
 		});
+		this.iconPos = vec(0, 0);
 	}
 	placeIcon(x: number, y: number) {
-		this.icon.pos = vec(x, y);
+		this.iconPos = vec(x, y);
+		this.icon.pos = this.iconPos.clone();
 	}
 }
 
